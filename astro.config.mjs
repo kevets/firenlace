4c709e467f72b8f2c57e8450afd8e798672ba4f8
Steve King <kevets@gmail.com>
2026-08-11 21:27:41 -0500

    Update astro.config.mjs
    fnl update2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,5 @@
-// @ts-check
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
+import { defineConfig } from 'astro';
 
-import cloudflare from "@astrojs/cloudflare";
-
-// https://astro.build/config
 export default defineConfig({
-	site: "https://example.com",
-	integrations: [mdx(), sitemap()],
-	adapter: cloudflare({
-		platformProxy: {
-			enabled: true,
-		},
-	}),
+  output: 'static'
 });
